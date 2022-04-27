99f44cbd6f3f0842c3752e603c60330183b50113
hoaison <abc@gmail.com>
2022-04-27 22:42:41 +0700

    hitpoint timer

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+// timer
+var seconds = 00;
+var tens = 00;
+var appendSeconds = document.getElementById('seconds');
+var appendTens = document.getElementById('tens');
+var interval;
+
+function startTimer() {
+   tens++;
+   if (tens < 9){
+      appendTens.innerHTML = '0' + tens;
+   }
+   if (tens > 9){
+      appendTens.innerHTML = tens;
+   }
+   if (tens > 99){
+      seconds++;
+      appendSeconds.innerHTML = '0'+seconds;
+      tens = 0;
+      appendTens.innerHTML = '0' + 0;
+   }
+   if (seconds > 9){
+      appendSeconds.innerHTML = seconds;
+   }
+}
+
+
 // set up canvas
 const para = document.querySelector('p');
+const hitpointValue = document.querySelector('.hpvalue');
+const hpMAX = 1000;
+let hitpoint = hpMAX;
 let count = 0;
 
 const canvas = document.querySelector('canvas');
@@ -11,10 +41,41 @@ const container = document.querySelector('.container');
 const width = canvas.width = container.clientWidth;
 const height = canvas.height = container.clientHeight;
 
+
+let speed = 2;
+function increaseValue() {
+   var value = parseInt(document.getElementById('number').value, 10);
+   value = isNaN(value) ? 0 : value;
+   value++;
+   document.getElementById('number').value = value;
+   speed = document.getElementById('number').value;
+}
+
+function decreaseValue() {
+   var value = parseInt(document.getElementById('number').value, 10);
+   value = isNaN(value) ? 0 : value;
+   value < 1 ? value = 1 : '';
+   value--;
+   document.getElementById('number').value = value;
+   speed = document.getElementById('number').value;
+}
+
+// asteroid speed
+
+
+console.log(speed);
+
+
+
 // function to generate random number
 
 function random(min, max) {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
+   const result = Math.floor(Math.random() * (max - min + 1)) + min;
+   if (result !== 0) {
+      return result;
+   } else {
+      return random(min, max);
+   }
 }
 
 // function to generate random RGB color value
@@ -50,6 +111,9 @@ class Ball extends Shape {
       ctx.fillStyle = this.color;
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
       ctx.fill();
+      ctx.strokeStyle = 'white';
+      ctx.lineWidth = .3;
+      ctx.stroke();
    }
 
    update() {
@@ -75,7 +139,13 @@ class Ball extends Shape {
             const distance = Math.sqrt(dx * dx + dy * dy);
 
             if (distance < this.size + ball.size) {
-               ball.color = this.color = randomRGB();
+               // this.color = randomRGB();
+
+               // this.velX = - this.velX;
+               // this.velY = - this.velY;
+               // ball.velX = - ball.velX;
+               // ball.velY = - ball.velY;
+
             }
          }
       }
@@ -106,7 +176,7 @@ class EvilCircle extends Shape {
       ctx.lineWidth = 3;
       ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
       ctx.stroke();
-      ctx.fillStyle = 'rgba(25, 25, 25, 0.8)';
+      ctx.fillStyle = 'rgba(25, 25, 25, 1)';
       ctx.fill();
    }
 
@@ -140,8 +210,12 @@ class EvilCircle extends Shape {
                this.size += 0.8;
                this.color = randomRGBEvilCircle();
                count--;
-               // 50;
-               para.textContent = `Asteroid count: ${count}`
+               para.textContent = `Asteroids: ${count}`;
+               
+               hitpoint--;
+               hitpointValue.textContent = `HP: ${(hitpoint/hpMAX*100).toFixed(0)}%`;
+               document.getElementById('hp').value=hitpoint/hpMAX*100;
+               
             }
          }
       }
@@ -152,21 +226,24 @@ class EvilCircle extends Shape {
 const balls = [];
 let ballNumber = window.prompt('How many asteroids? (max: 999)', 50);
 while (balls.length < ballNumber) {
+   //asteroid size
    const size = random(10, 20);
    const ball = new Ball(
       // ball position always drawn at least one ball width
       // away from the edge of the canvas, to avoid drawing errors
       random(0 + size, width - size),
       random(0 + size, height - size),
-      random(-5, 1) || random(1, 5),
-      random(-5, 1) || random(1, 5),
+      // initial speed
+      random(-speed, speed),
+      random(-speed, speed),
       randomRGB(),
       size
+
    );
 
    balls.push(ball);
    count++;
-   para.textContent = `Asteroid count: ${count}`
+   para.textContent = `Asteroids: ${count}`
 }
 
 const evil = new EvilCircle(
@@ -175,20 +252,39 @@ const evil = new EvilCircle(
 );
 
 function loop() {
-   ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
-   ctx.fillRect(0, 0, width, height);
-
-   for (const ball of balls) {
-      if (ball.exists) {
-         ball.draw();
-         ball.update();
-         ball.collisionDetect();
-      }
+   if (count === 0) {
+      container.classList.remove('active');
+      clearInterval(interval);
+      alert('The black hole has taken the entire galaxy!!!');
+      
+      
    }
-   evil.draw();
-   // evil.checkBounds();
-   evil.collisionDetect();
-   requestAnimationFrame(loop);
+   else if (hitpoint <= 0){
+      clearInterval(interval);
+      container.classList.remove('active');
+      alert('GAME OVER!!! Too many collisions! The black hole lost all health!!!');
+      
+   }
+   else {
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+      ctx.fillRect(0, 0, width, height);
+
+      for (const ball of balls) {
+         if (ball.exists) {
+            ball.draw();
+            ball.update();
+            ball.collisionDetect();
+         }
+      }
+      evil.draw();
+      // evil.checkBounds();
+      evil.collisionDetect();
+      requestAnimationFrame(loop);
+      console.log(random(-5, 5) === 0);
+
+   }
+
+
 }
 
 
@@ -198,11 +294,15 @@ const reset = document.querySelector('.reset');
 const startGame = document.querySelector('.start');
 startGame.addEventListener('click', (e) => {
 
-      e.preventDefault();
-      container.classList.add('active');
-      startGame.classList.add('hidden');
-      reset.classList.add('show');
-      loop();
+   e.preventDefault();
+   container.classList.add('active');
+   startGame.classList.add('hidden');
+   reset.classList.add('show');
+   interval = setInterval(startTimer);
+
+
+   loop();
+
 });
 
 
